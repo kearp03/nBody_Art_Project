32722368b4f3df848fe120b1a29b228e1de99f5b
Kyle Earp <kearp03@centurylink.net>
2024-11-26 13:29:27 -0600

    Mess around with different colors and initial positions

--- ProjectnBodyArt.cu
+++ ProjectnBodyArt.cu
@@ -528,9 +528,9 @@ void setSimulationParameters()
 
 	// This is a lennard-Jones type force G*m1*m2/(r^2) - H*m1*m2/(r^4).
 	// If you want a gravity type force just set G to your gravity and set H equal 0.
-	G = 0.6;
+	G = 0.4;
 
-	H = 0.03;
+	H = 0.02;
 
 	dForce = 0.9;
 
